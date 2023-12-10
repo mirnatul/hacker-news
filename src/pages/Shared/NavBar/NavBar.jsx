eb413ee883fba41a44e4a9b6d6f640bc56aa2bf0
Li Pon <mirnatul@gmail.com>
2023-12-10 18:07:44 +0600

    basic navbar fixed

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 const NavBar = () => {
     return (
-        <div>
-            this is navbar
+        <div className="navbar bg-base-200">
+            <div className="navbar-start">
+                <a className="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+            <div className="navbar-end">
+                {/* <button className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                </button> */}
+                {/* search field */}
+                <div className="form-control">
+                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                </div>
+            </div>
         </div>
     );
 };
